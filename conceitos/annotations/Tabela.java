87d7ef0f2e77fda6f6da09fc06a2cb5edeed3230
Maísa Rabelo <103072055+Maisarbo@users.noreply.github.com>
2023-06-13 15:58:52 -0300

    Delete Tabela.java

--- conceitos/annotations/Tabela.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.lang.annotation.*;
-
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@Inherited
-public @interface Tabela {
-    public String table();
-}
